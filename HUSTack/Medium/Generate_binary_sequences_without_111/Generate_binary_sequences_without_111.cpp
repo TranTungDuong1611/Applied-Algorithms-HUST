022576ec3f4754e6ffca3c883d0320e91d4e015d
tungduong123q@gmail.com <tungduong123q@gmail.com>
2025-04-30 21:40:36 +0700

    BackTracking

---
+++ HUSTack/Medium/Generate_binary_sequences_without_111/Generate_binary_sequences_without_111.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+int arr[1000];
+
+void input(){
+    cin >> n;
+}
+
+void printSol(){
+    for(int i=1; i<=n; i++) cout << arr[i];
+    cout << endl;
+}
+
+void Try(int k){
+    for(int i=0; i<=1; i++){
+        arr[k] = i;
+        if(k < 3) Try(k+1);
+        else{
+            if(arr[k] == 1 && arr[k-1] == 1 && arr[k-2] == 1);  // invalid case
+            else{
+                if(k == n) printSol();
+                else Try(k+1);
+            }
+        }
+    }
+}
+
+int main(){
+    input();
+    Try(1);
+    return 0;
+}
---
+++ HUSTack/Medium/Generate_binary_sequences_without_111/README.md
@@ -0,0 +1,38 @@
+# Description
+Write a program that generates all binary sequence of length n that do not contains 111.
+## Input
+Line 1: contains a positive integer n 
+
+## Output
+Write in each line a binary sequence containing no 111 (the sequences are listed in a lexicographic order)
+
+## Example
+    Input 
+    3
+
+    Output 
+    000
+    001
+    010
+    011
+    100
+    101
+    110
+
+    Input 
+    4
+
+    Output 
+    0000
+    0001
+    0010
+    0011
+    0100
+    0101
+    0110
+    1000
+    1001
+    1010
+    1011
+    1100
+    1101
